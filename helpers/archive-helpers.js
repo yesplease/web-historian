6323723a7a4dcc86828f399464cae2b647cb9edf
yesplease <bradley.heinz@gmail.com>
2015-02-25 20:06:52 -0800

    Add isURLInList functionality

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var request = require('http-request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -25,17 +26,26 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfURLs = function(){
 };
 
-exports.isUrlInList = function(){
+exports.isURLInList = function(url){
+  // url = "'"+url+"'";
+  return fs.readFile(exports.paths.list, 'utf8', function(err, data){
+    if (err) throw err;
+    // console.log(data.split('\n'));
+    // console.log("url to string after splitting: ", data.split('\n'), ['www.facebook.com'], url);
+    console.log( 'www.facebook.com' === data.split('\n')[2] );
+
+    return _.contains( testdata, url);
+  });
 };
 
-exports.addUrlToList = function(){
+exports.addURLToList = function(){
 };
 
 exports.isURLArchived = function(){
 };
 
-exports.downloadUrls = function(){
+exports.downloadURLs = function(){
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -6,24 +6,51 @@ var request = require('http-request');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  if ( req.method === 'GET' ) {
-    console.log("This is the request url", req.url);
-    // if root path -> /index.html (response 301)
+  var assetPath = archive.paths.siteAssets;
 
+  if ( req.method === 'GET' ) {
+    // console.log("This is the request url", req.url);
     //case 1: is it the root?
+    // if so, serve it up (serveAssets)
     if ( req.url === '/' ) {
-      var urlPath = archive.paths.siteAssets + '/index.html';
-      console.log("This is our urlPath", urlPath);
-      fs.readFile(urlPath, function (err, data) {
+      assetPath += '/index.html';
+      console.log("This is our urlPath", assetPath);
+      fs.readFile(assetPath, function (err, data) {
         if (err) throw err;
         res.writeHead(200, httpHelp.headers);
         res.end(data);
       });
     }
 
+    //look at incoming URL to determine if it is a valid archive query?
+    //if yes,
+    if ( req.url.substr(0, 6) === '/?url=' ) {
+      // check if query site is in sites.txt
+      var queryURL = req.url.substr(6);
+      // console.log('valid query:', queryURL);
+      console.log('is it in the list?', archive.isURLInList(queryURL) );
+
+      if ( archive.isURLInList(queryURL) ) {
+        //if yes:
+          // serve it up (serveAssets)
+          console.log('archive is in list!');
+      }
+        // if not
+          // add to queue
+          // archive.addURLToList()
+
+          // serve up loading page (come back later)
+    //if not, is it a site asset that we have (e.g. index.html)
+    } else {
+      //if it is, serve that up (serveAssets)
+      //if not, serve up a 404!
+    }
 
     //case 2: is it for a site asset? (web/public)
-    if ( req.url === '/index.html' ) {
+    // if so  (serveAssets)
+      // assetPath +=  // plus our path
+
+    if ( /* path exists */ true ) {
       fs.readFile('./public/index.html', function (err, data) {
         if (err) throw err;
         res.writeHead(200, httpHelp.headers);
@@ -34,11 +61,6 @@ exports.handleRequest = function (req, res) {
     //case 3: is it for an archived site? (achives/sites)
     // /www.google.com ???
 
-    // if site is in sites.txt
-      // serve it up
-    // if not
-      // add to queue/create it
-      // serve up loading page (come back later)
   }
 
   // res.end(archive.paths.list);
